5fc8a6970f7ea172b6ca7b2181fadb3c67a90d1c
Walisson Ferreira <ferreira.walisson@trezo.com.br>
2019-10-09 16:12:59 -0300

    Adicionada carga inicial do banco

--- appinit.sh
+++ appinit.sh
@@ -1,4 +1,16 @@
+#!/bin/bash
 pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
+
+importado="db/.importado"
+
+if [ -f "$importado" ]
+then
+		echo "Carga inicial de dados ja efetuada, continuando..."
+else
+		echo "Carga inicial de dados sendo efetuada, aguarde, pode demorar um pouco..."
+		python manage.py loaddata db/dados_iniciais.json
+		echo 'ok' > db/.importado
+fi
 python manage.py runserver 0.0.0.0:8000

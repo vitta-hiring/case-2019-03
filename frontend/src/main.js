7eb6870d9153f735033349442e6c0c549cde3000
Rafael Soufraz <soufraz@gmail.com>
2019-10-13 19:53:27 -0300

    Eslint in frontend src folder

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,20 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from "./router";
+import router from './router'
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.use(ElementUI)
-
 import axios from 'axios'
+
+Vue.use(ElementUI)
 
 // allow use http client without Vue instance
 const http = axios.create({
-  baseURL: 'http://0.0.0.0:3333',
+  baseURL: 'http://0.0.0.0:3333'
 })
 
-Vue.prototype.$http = http;
+Vue.prototype.$http = http
 
 new Vue({
   el: '#app',
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,5 @@
-import Vue from "vue"
-import Router from "vue-router"
+import Vue from 'vue'
+import Router from 'vue-router'
 
 import NewPrescription from '../NewPrescription'
 import Doctors from '../Doctors'
@@ -8,24 +8,24 @@ import Patients from '../Patients'
 Vue.use(Router)
 
 const router = new Router({
-    routes: [
-        {
-            path: '/new-prescription',
-            name: 'NewPrescription',
-            component: NewPrescription
-        },
-        {
-            path: '/doctors',
-            name: 'Doctors',
-            component: Doctors
-        },
-        {
-            path: '/patients',
-            name: 'Patients',
-            component: Patients
-        }
-    ],
-    mode: 'history'
+  routes: [
+    {
+      path: '/new-prescription',
+      name: 'NewPrescription',
+      component: NewPrescription
+    },
+    {
+      path: '/doctors',
+      name: 'Doctors',
+      component: Doctors
+    },
+    {
+      path: '/patients',
+      name: 'Patients',
+      component: Patients
+    }
+  ],
+  mode: 'history'
 })
 
 router.replace({ path: 'new-prescription', redirect: '/new-prescription' })

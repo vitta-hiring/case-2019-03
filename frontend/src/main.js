f2c674f45047f143b306f52f3a2b294ad506331e
Rafael Soufraz <soufraz@gmail.com>
2019-10-14 23:45:57 -0300

    Portuguese language vuejs

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,10 +4,11 @@ import router from './router'
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/pt'
 
 import axios from 'axios'
 
-Vue.use(ElementUI)
+Vue.use(ElementUI, { locale })
 
 // allow use http client without Vue instance
 const http = axios.create({

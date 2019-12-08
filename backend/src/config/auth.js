76884ab736ef255f51971e3b9ad62bd9d6096180
Allan <allan.jre@gmail.com>
2019-12-08 10:57:02 -0300

    Desafio Vitta, entregue as API's de Usuario, Paciente e Sessão (User, Patient and Session)

---
+++ backend/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '0f35074a1d12011b0bbad506fbe64528',
+  expiresIn: '7d'
+}
---
+++ backend/src/database/migrations/20191208065200-create-patients.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('patients', { 
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      age: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      gender: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('patients');
+  }
+};

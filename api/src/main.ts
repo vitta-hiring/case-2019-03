5ebfdeae870f92170d8eb1799cccebde8e63cc58
lucasferreiralsf <lucasferreiralsf@hotmail.com>
2020-02-26 08:17:51 -0300

    Docker compose and .env config

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors: { origin: '*', methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS']}});
   await app.listen(3123);
 }
 bootstrap();

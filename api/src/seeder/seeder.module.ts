3307546cf6ce5247699872e57f306011e7d54018
lucasferreiralsf <lucasferreiralsf@hotmail.com>
2020-02-27 02:50:30 -0300

    Seeder module fix.

--- api/src/seeder/seeder.module.ts
+++ api/src/seeder/seeder.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { DrugModule } from '../drug/drug.module';
+import { DrugInteractionModule } from '../drug-interaction/drug-interaction.module';
+import { MedicineModule } from '../medicine/medicine.module';
 import { SeederService } from './seeder.service';
 
 @Module({
+  imports: [DrugModule, DrugInteractionModule, MedicineModule],
   providers: [SeederService]
 })
 export class SeederModule {}

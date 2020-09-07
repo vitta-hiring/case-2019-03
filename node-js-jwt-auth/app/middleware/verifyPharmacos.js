b34adaf5330b438bfe6fb9d2eb7e9957f08ea043
Raniel Mendonça <raniel@MacBook-Pro-de-Raniel.local>
2020-09-07 03:21:50 -0300

    Bugfix verify pharmacos

--- node-js-jwt-auth/app/middleware/verifyPharmacos.js
+++ node-js-jwt-auth/app/middleware/verifyPharmacos.js
@@ -1,57 +1,64 @@
 const db = require("../models");
-const Pharmacos = db.pharmaco;
+const Pharmaco = db.pharmaco;
 const User = db.user;
 
 checkDuplicateUsernameOrEmail = (req, res, next) => {
-  // Username
-  User.findOne({
-    where: {
-      username: req.body.username
-    }
-  }).then(user => {
-    if (user) {
-      res.status(400).send({
-        message: "Failed! Username is already in use!"
-      });
-      return;
-    }
-
-    // Email
+    // Username
     User.findOne({
-      where: {
-        email: req.body.email
-      }
+        where: {
+            username: req.body.username
+        }
     }).then(user => {
-      if (user) {
-        res.status(400).send({
-          message: "Failed! Email is already in use!"
-        });
-        return;
-      }
+        if (user) {
+            res.status(400).send({
+                message: "Failed! Username is already in use!"
+            });
+            return;
+        }
 
-      next();
+        // Email
+        User.findOne({
+            where: {
+                email: req.body.email
+            }
+        }).then(user => {
+            if (user) {
+                res.status(400).send({
+                    message: "Failed! Email is already in use!"
+                });
+                return;
+            }
+
+            next();
+        });
     });
-  });
 };
 
-checkRolesExisted = (req, res, next) => {
-  if (req.body.roles) {
-    for (let i = 0; i < req.body.roles.length; i++) {
-      if (!ROLES.includes(req.body.roles[i])) {
-        res.status(400).send({
-          message: "Failed! Role does not exist = " + req.body.roles[i]
+checkPharmacosExisted = (req, res, next) => {
+    Pharmaco.findAll({}).then(pharmacos => {
+        namesPharmacos = [];
+        pharmacos.forEach(pharmaco => {
+            namesPharmacos.push(pharmaco.dataValues.name);
         });
-        return;
-      }
-    }
-  }
-  
-  next();
+        if (req.body.pharmacos) {
+            for (let i = 0; i < req.body.pharmacos.length; i++) {
+                console.log(req.body.pharmacos[i]);
+                if (!namesPharmacos.includes(req.body.pharmacos[i])) {
+                    // console.log(pharmacos);
+                    res.status(400).send({
+                        message: "Failed! Pharmaco does not exist = " + req.body.pharmacos[i]
+                    });
+                    return;
+                }
+            }
+            next();
+        }
+    });
 };
 
-const verifySignUp = {
-  checkDuplicateUsernameOrEmail: checkDuplicateUsernameOrEmail,
-  checkRolesExisted: checkRolesExisted
+const verifyPharmacos = {
+    checkDuplicateUsernameOrEmail: checkDuplicateUsernameOrEmail,
+    checkPharmacosExisted: checkPharmacosExisted
 };
 
-module.exports = verifySignUp;
+module.exports = verifyPharmacos;
